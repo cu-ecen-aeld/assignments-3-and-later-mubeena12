b724ae8ca951a35fe18b75d6e9b1649e3e8e609c
mubeena <mubeena@Ubuntu20.04.myguest.virtualbox.org>
2023-09-02 22:11:54 -0600

    Assignment1: Adding finder.sh and writer.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+filesdir="$1"
+searchstr="$2"
+
+# Check number of arguments
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <provide file path> <provide search string>"
+  exit 1
+fi
+
+# Check if specified directory exist
+if [ ! -d "$filesdir" ]; then 
+       echo "Error: '$filesdir' is not a directory or does not exist"
+       exit 1
+fi       
+
+# Function to search for the text string in files and count matching lines
+string_search(){
+  local dir="$1"
+  local search="$2"
+  local number_of_files=0
+  local number_of_line_count=0
+
+  local all_files=($(find "${dir}" -type f))
+  for file in ${all_files[@]}; do
+    if [ -f "$file" ]; then
+      number_of_files=$((number_of_files + 1))
+      matching_lines=$(grep -c "$search" "$file")
+      number_of_line_count=$((number_of_line_count + matching_lines))
+    fi
+  done
+
+  echo "The number of files are $number_of_files and the number of matching lines are $number_of_line_count"
+}
+
+# Call the function to search 
+string_search "$filesdir" "$searchstr"
---
+++ finder-app/writer.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+writefile="$1"
+writestr="$2"
+
+# Check number of arguments
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <Provide a write file> <Provide a text to write in the file>"
+	exit 1
+fi
+
+# Check writefile is specified
+if [ -z "$writefile" ]; then
+  echo "Error: 'writefile' argument is not specified."
+  exit 1
+fi
+
+# Check writestr is specified
+if [ -z "$writestr" ]; then
+  echo "Error: 'writestr' argument is not specified."
+  exit 1
+fi
+
+# Create directory
+mkdir -p "$(dirname "$writefile")"
+
+# Write the content to the file
+echo "$writestr" > "$writefile"
+
+# Check for error
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to write to '$writefile'."
+  exit 1
+fi
+
+echo "Content has been successfully written to '$writefile'."
